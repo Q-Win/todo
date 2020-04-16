e74200db1815218fcc362a658afd35847d9951c8
Quinn Krug <quinn.krug@gmail.com>
2020-04-15 18:24:10 -0600

    add flash msg

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -4,4 +4,20 @@ class TodosController < ApplicationController
     @todo = Todo.new
   end
 
+  def create
+    @todo = Todo.new(todo_params)
+    if @todo.save
+      flash[:notice] = "Todo was created successfully"
+      redirect_to todo_path(@todo)
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def todo_params
+    params.require(:todo).permit(:name, :description)
+  end
+
 end
